eb261b4e03fb58729120876b352b924291b01a4d
Edward Boland <mr.eboland@gmail.com>
2021-05-05 19:21:32 -0400

    practice q for if let

--- src/main.rs
+++ src/main.rs
@@ -124,4 +124,51 @@ fn main() {
 
     assert_eq!(Some("dog").unwrap_or("cat"), "dog");
     assert_eq!(None.unwrap_or("cat"), "cat");
+
+
+    // Exercise 1:
+
+    let john = Person {
+        first: String::from("James"),
+        middle: Some(String::from("Oliver")),
+        last: String::from("Smith"),
+    };
+    assert_eq!(build_full_name(&john), "James Oliver Smith");
+
+    let alice = Person {
+        first: String::from("Alice"),
+        middle: None,
+        last: String::from("Stevens"),
+    };
+    assert_eq!(build_full_name(&alice), "Alice Stevens");
+
+    let bob = Person {
+        first: String::from("Robert"),
+        middle: Some(String::from("Murdock")),
+        last: String::from("Jones"),
+    };
+    assert_eq!(build_full_name(&bob), "Robert Murdock Jones");
+
+}
+
+struct Person {
+    first: String,
+    middle: Option<String>,
+    last: String,
+}
+
+fn build_full_name(person: &Person) -> String {
+    let mut full_name = String::new();
+    full_name.push_str(&person.first);
+    full_name.push_str(" ");
+
+    // TODO: Implement the part of this function that handles the person's middle name.
+    // if let is checking for the pattern middle and checking it against the expression &person.middle. If the latter has a middle, the if block is executed.
+    if let Some(middle) = &person.middle {
+        full_name.push_str(&middle);
+        full_name.push_str(" ");
+    }
+
+    full_name.push_str(&person.last);
+    return full_name
 }
