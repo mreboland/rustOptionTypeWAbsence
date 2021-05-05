fa762b4cf53b7858bf23479a7c8fe0eb36bfface
Edward Boland <mr.eboland@gmail.com>
2021-05-05 18:38:01 -0400

    pattern matching

--- src/main.rs
+++ src/main.rs
@@ -35,4 +35,36 @@ fn main() {
     // The first two prints get us Some("banana") and Some("coconut"). The third one returned a None value (which isn't associated with any data) instead of panicking if we didn't use get.
 
     // In practice, we must decide how our program behaves depending on what enum variant it gets. Now on how to access data inside Some(data).
+
+    // PATTERN MATCHING
+
+    // Rust has an extremely powerful control flow operator called match, which we can use to compare a value against a series of patterns and then execute code based on which pattern matches.
+
+    let fruits = vec!["banana", "apple", "coconut", "orange", "strawberry"];
+
+    // Explanation for '&' from https://stackoverflow.com/questions/57339201/what-is-the-purpose-of-before-the-loop-variable
+    // So normally when you use for i in list, the loop variable i would be of type &i32.
+    // But when instead you use for &i in list, you are not dereferencing anything, but instead you are using pattern matching to explicitly destructure the reference and that will make i just be of type i32.
+    for &index in [0, 2, 99].iter() {
+        match fruits.get(index) {
+            Some(fruit_name) => println!("it's a delicious {}", fruit_name),
+            None => println!("There is no fruit! :("),
+        }
+    };
+
+    // In the above code we iterate through the same indexes from our previous example (0, 2, and 99) and then use each one to retrieve a value from the fruits vector by using the fruits.get(index) expression.
+
+    // Because the fruits vector contains &str elements, we know that the result of this expression is of type Option<&str> (I believe this is why we need & before our variable index so that we are explicitly matching the elements &index and &str). We then use a match expression against the Option value and define a course of action for each of its variants. Rust refers to those branches as match arms, and each arm can handle on possible outcome for the matched value.
+
+    // We can refine our match expression even further to act differently, depending on the values inside a Some variant. For example, we could stress the fact the coconuts are awesome by running the following:
+
+    for &index in [0, 2, 99].iter() {
+        match fruits.get(index) {
+            Some(&"coconut") => println!("Coconuts are awesome!!!"),
+            Some(fruit_name) => println!("It's a delicious {}", fruit_name),
+            None => println!("there is no fruits! :("),
+        };
+    };
+
+
 }
